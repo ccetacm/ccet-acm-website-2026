8075d64af11a385cb813cc8de310cb53b92ee900
bhavyadmn05 <bhavyadmn05@gmail.com>
2025-08-05 01:10:49 +0530

    website loader changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-// src/App.jsx
-import React, { useState } from "react";
-import Loader from "./components/loader";
+import React from "react";
+import LoaderAndContent from "./components/loader";
 
-export default function App() {
-  const [showContent, setShowContent] = useState(false);
-
+const App = () => {
   return (
-    <>
-      {!showContent && <Loader onFinish={() => setShowContent(true)} />}
-      <div
-        id="website-content"
-        className={`website-content ${
-          showContent ? "website-content-visible" : "website-content-hidden"
-        }`}
-        style={{
-          padding: 20,
-          textAlign: "center",
-          fontSize: "1.2em",
-          color: "#333",
-        }}
-      >
-        <h1>Welcome to your website!</h1>
-        <div
-          style={{
-            height: 1000,
-            backgroundColor: "#f0f8ff",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <p>More content here to test scrolling!</p>
-        </div>
-      </div>
-    </>
+    <div>
+      <LoaderAndContent />
+    </div>
   );
-}
+};
+
+export default App;
