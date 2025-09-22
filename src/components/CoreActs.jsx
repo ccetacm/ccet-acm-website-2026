91971dd747b4bcd9be973970904ed39673a65f7d
Abhay-1704 <abhaysps2004@gmail.com>
2025-09-22 15:21:47 +0530

    Grid

--- src/components/CoreActs.jsx
+++ src/components/CoreActs.jsx
@@ -103,17 +103,6 @@ const CoreActs = () => {
                     <div className={styles.cardFooter}>
                       <button className={styles.learnMoreButton}>
                         Learn More
-                        <svg
-                            className={styles.buttonIcon}
-                            width="16"
-                            height="16"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                        >
-                          <path d="M5 12h14M12 5l7 7-7 7"/>
-                        </svg>
                       </button>
                     </div>
                   </div>
