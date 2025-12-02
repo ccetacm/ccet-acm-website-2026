53b5f572786969adcc633724054551625cd629bb
Abhay-1704 <abhaysps2004@gmail.com>
2025-12-02 12:45:03 +0530

    Changed Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import App from "./App.jsx";
 import About from "./pages/About.jsx";
